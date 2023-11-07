80f1d2bd68795d9316fbcf590af735fc3c98762e
Gabriel <gabriel.krapp@hotmail.com>
2023-11-06 22:00:59 -0300

    Installing Cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
